2caa1541580fbf0fe5136f4f793a1a43bdf75510
jakeh1 <jacobmhenrichs@gmail.com>
2021-11-30 16:51:10 -0600

    Stuff

---
+++ WebApplication2/Controllers/LoginController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+        private void HashPassword(string password)
+        {
+
+        }
+
+        private string GeneratePassword()
+        {
+            string password = "";
+            Random random = new Random();
+            for(int i = 0; i < 15; i++)
+            {
+                int numOrLeter = random.Next(0, 1);
+                char randomChar;
+                if(numOrLeter == 1) //numbers
+                {
+                    randomChar = Convert.ToChar(random.Next(48,57));
+                }
+                else //letters
+                {
+                    if(random.Next(0,1) == 0) //upper case
+                    {
+                        randomChar = Convert.ToChar(random.Next(65, 90));
+                    }
+                    else //lower case
+                    {
+                        randomChar = Convert.ToChar(random.Next(97, 122));
+                    }
+                    
+                }
+                password += randomChar;
+            }
+            return password;
+        }
+        private void SendPassword(string password)
+        {
+
+        }
+    }
+}
---
+++ WebApplication2/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string session { get; set; }
+    }
+}
