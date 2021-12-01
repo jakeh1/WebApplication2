40c93b6680f6c7bbe256168826e147779ba94ee1
Jacob Henrichs <henrichsj@uwplatt.edu>
2021-12-01 13:03:51 -0600

    Added some more stuff I still have to fix how xml is read in and write some more xml reads

--- WebApplication2/Controllers/LoginController.cs
+++ WebApplication2/Controllers/LoginController.cs
@@ -3,27 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
     public class LoginController : Controller
     {
+        private const string PATH = "";
+        
         // GET: Login
         public ActionResult Index()
         {
             return View();
         }
 
-
-
-
-
-
-
-
-        private void HashPassword(string password)
+        //When the username is submited
+        [HttpPost]
+        public ActionResult LoginUsernameHistory(FromCollection collection)
         {
-
+            List<UserModel> users = GetUsers();
+            string username = "";
+            int uId = -1;
+            foreach(UserModel user in users)
+            {
+                if(user.UserName == collection["username"])
+                {
+                    username = user.UserName;
+                    uId = user.id;
+                    break;
+                }
+            }
+            if(username == "") 
+            {
+                //Return error message
+            }
+            else
+            {
+                string tempPass = GeneratePassword();
+                string passHash = HashPassword(tempPass);
+                SendPassword(uId, tempPass);
+                //set password hash to xml file
+                //Return the password login view
+            }
+            
+        }
+        //when the password is submited
+        [HttpPost]
+        public ActionResult LoginPasswordHistory(FromCollection collection)
+        {
+            List<UserModel> users = GetUsers();
+            foreach(UserModel user in users)
+            {
+                if(user.UserName == collection["username"] && user.PasswordHash == HashPassword(collection["password"]))
+                {
+                    //sends back the history view with the right stuff
+                    break;
+                }
+                else
+                {
+                    //sends back error message with view.
+                    //send back to start of login process with password being regentrated
+                }
+            }
+        }
+        private string HashPassword(string password)
+        {
+            var sha1 = SHA1.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(value);
+            var hash = sha1.ComputeHash(inputBytes);
+            var stringBuilder = new StringBuilder();
+            
+            for (var i = 0; i < hash.Length; i++)
+            {
+                stringBuilder.Append(hash[i].ToString("X2"));
+            }
+            string hashPass = stringBuilder.ToString();
+            //Todo send hashed password to the file
+            return hashPass;
         }
 
         private string GeneratePassword()
@@ -54,7 +110,17 @@ namespace WebApplication2.Controllers
             }
             return password;
         }
-        private void SendPassword(string password)
+        private void SendPassword(string username, string password)
+        {
+            //TODO open file and save to it
+        }
+        //Clears out the password and the password hash.
+        private void ClearPassword(int uId)
+        {
+        }
+
+        //Reads in the user data from the xml file
+        private List<UserModel> GetUsers()
         {
 
         }
--- WebApplication2/Models/UserModel.cs
+++ WebApplication2/Models/UserModel.cs
@@ -8,7 +8,8 @@ namespace WebApplication2.Models
     public class UserModel
     {
         public string UserName { get; set; }
-        public string Password { get; set; }
-        public string session { get; set; }
+        public string PasswordHash { get; set; }
+        public int id { get; set; }
+        
     }
 }
